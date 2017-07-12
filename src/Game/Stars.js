129c4acd1732d50820781c9962a6d3b554131e0d
Luke Belfield <Luke.Belfield@dealogic.com>
2017-07-12 11:00:10 +0100

    generate random stars

--- src/Game/Stars.js
+++ src/Game/Stars.js
@@ -2,14 +2,23 @@ import React from 'react';
 import './Stars.css';
 
 const Stars = (props) => {
+
+    // Math.random()*9 generates a random number between 0 and 9
+    // however, we don't want 0, so we add a 1 to this. We then have to wrap this in a Math.floor() 
+    const numberOfStars = 1 + Math.floor(Math.random()*9);
+
+    let starArray = [];
+
+    for(let i=0; i < numberOfStars; i++) {
+        // pushes the html to the starArray
+        starArray.push(<i key={i} className="fa fa-star"></i>);
+    }
+
     return (
-    <div className="col-5">
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-    </div>
+        // renders the starArray that contains html.
+        <div className="col-5">
+            {starArray}
+        </div>
     );   
 }
 
